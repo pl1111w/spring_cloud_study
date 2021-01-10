ded4d89fe0d021c523255d3356e8387b856f2631
Xiaobo <1362596047@qq.com>
2021-01-10 20:02:09 +0800

    eureka集群

---
+++ cloulds/cloud-eureka-server7002/src/main/java/com/pl1111w/springcloud/EurekaMain7002.java
@@ -0,0 +1,20 @@
+package com.pl1111w.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @title: pl1111w
+ * @description: TODO
+ * @author: Kris
+ * @date 2021/1/10 19:47
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
