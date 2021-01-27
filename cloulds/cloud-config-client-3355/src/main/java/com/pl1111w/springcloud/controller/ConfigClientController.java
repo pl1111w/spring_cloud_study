14eb4449b8031c4d3efc452dc1a678aa481c80fe
Xiaobo <1362596047@qq.com>
2021-01-27 21:16:21 +0800

    spring配置中心读取-手动版

--- cloulds/cloud-config-client-3355/src/main/java/com/pl1111w/springcloud/controller/ConfigClientController.java
+++ cloulds/cloud-config-client-3355/src/main/java/com/pl1111w/springcloud/controller/ConfigClientController.java
@@ -1,16 +1,18 @@
 package com.pl1111w.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @title: pl1111w
- * @description: TODO
+ * @description: crul -X POST "http://localhost:3355/actuator/refresh"
  * @author: Kris
  * @date 2021/1/26 22:49
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
