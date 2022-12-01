f67ed23416c216ac0c1374f1db8b51d242de2826
xiaobo wei <1362596047@qq.com>
2022-12-01 14:20:11 +0800

    seata

--- cloulds/cloudalibaba-seata-order-service2001/src/main/java/com/pl1111w/springcloud/config/MyBatisConfig.java
+++ cloulds/cloudalibaba-seata-order-service2001/src/main/java/com/pl1111w/springcloud/config/MyBatisConfig.java
@@ -30,10 +30,10 @@ public class MyBatisConfig {
      * @param sqlSessionFactory SqlSessionFactory
      * @return SqlSessionTemplate
      */
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
+//    @Bean
+//    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+//        return new SqlSessionTemplate(sqlSessionFactory);
+//    }
 
     /**
      * 从配置文件获取属性构造datasource，注意前缀，这里用的是druid，根据自己情况配置,
@@ -47,32 +47,32 @@ public class MyBatisConfig {
         return new DruidDataSource();
     }
 
-    /**
-     * 构造datasource代理对象，替换原来的datasource
-     *
-     * @param druidDataSource
-     * @return
-     */
-    @Primary
-    @Bean("dataSource")
-    public DataSourceProxy dataSourceProxy(DataSource druidDataSource) {
-        return new DataSourceProxy(druidDataSource);
-    }
-
-    @Bean(name = "sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactoryBean(DataSourceProxy dataSourceProxy) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSourceProxy);
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        bean.setMapperLocations(resolver.getResources(mapperLocations));
-
-        SqlSessionFactory factory;
-        try {
-            factory = bean.getObject();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return factory;
-    }
+//    /**
+//     * 构造datasource代理对象，替换原来的datasource
+//     *
+//     * @param druidDataSource
+//     * @return
+//     */
+//    @Primary
+//    @Bean("dataSource")
+//    public DataSourceProxy dataSourceProxy(DataSource druidDataSource) {
+//        return new DataSourceProxy(druidDataSource);
+//    }
+//
+//    @Bean(name = "sqlSessionFactory")
+//    public SqlSessionFactory sqlSessionFactoryBean(DataSourceProxy dataSourceProxy) throws Exception {
+//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+//        bean.setDataSource(dataSourceProxy);
+//        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+//        bean.setMapperLocations(resolver.getResources(mapperLocations));
+//
+//        SqlSessionFactory factory;
+//        try {
+//            factory = bean.getObject();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        return factory;
+//    }
 }
 
